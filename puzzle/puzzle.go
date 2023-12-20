da9b09190a8a674f8f0f319b84097075bce5a2e7
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-20 16:06:10 +0200

    refactor: alias the `byte` type used for the grid and use constants for the puzzle variables

--- puzzle/puzzle.go
+++ puzzle/puzzle.go
@@ -2,22 +2,28 @@ package puzzle
 
 import (
 	"errors"
+	"fmt"
 	"unicode"
 )
 
+type Place byte
+
 // TODO: It might be strange to use 'uint8' but let's try it since it uses
 // less memory.
-type Puzzle [][]byte
+type Puzzle [][]Place
+
+const Unresolved Place = 0
+const Len = 9
 
 func Parse(input []string) (Puzzle, error) {
 	var p Puzzle
 	for _, line := range input {
-		row := make([]byte, 0, len(line))
+		row := make([]Place, 0, len(line))
 		for _, c := range line {
 			if unicode.IsDigit(c) {
-				row = append(row, byte(c-'0'))
+				row = append(row, Place(c-'0'))
 			} else if c == '.' {
-				row = append(row, byte(0))
+				row = append(row, 0)
 			} else {
 				return nil, errors.New("Invalid input character '" + string(c) + "'")
 			}
